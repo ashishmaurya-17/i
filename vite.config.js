a7dcdaa893da86a789d104784bfc55f534dffcec
Ashish kumar <154320204+ashishmaurya-17@users.noreply.github.com>
2025-11-22 17:01:29 +0530

    Add Vite configuration for React project

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // This makes it work on any GitHub repo name automatically
+})
